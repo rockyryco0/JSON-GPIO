43a5d2844b1a17dda4e0610ed1ae2ebfebd0a438
AndrycoHut <48755450+rockyryco0@users.noreply.github.com>
2019-07-18 09:48:37 +0700

    Update views.py

--- raspi/raspiapp/views.py
+++ raspi/raspiapp/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 import json
 
 # Create your views here.
+# this is the setting for your JSON file using json module
 def datajson(request):
-    with open(r'C:\Users\ASUS ROG\PycharmProjects\little\Django\raspi\raspiapp\static\json_file.json') as json_data:
-        data1 = json.load(json_data) # deserialises itjson
+    with open(r'..\yourpathproject\raspi\raspiapp\static\json_file.json') as json_data:
+        data1 = json.load(json_data) # read the official page for detail information (https://docs.python.org/3/library/json.html)
 
     json_data.close()
 
+    # use 'data2' as a variable to pass to your html file
     return render(request, 'raspiapp/json.html', {'data2': data1})
+
