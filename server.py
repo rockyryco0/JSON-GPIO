cb02dddf5a8d212c47d0950341708092ef52e283
AndrycoHut <48755450+rockyryco0@users.noreply.github.com>
2019-07-18 09:56:05 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,33 +3,36 @@
 from socket import *
 import sys
 import json
-import re
 
+# creates file when the file is doesn't exists
 try:
     open("json_file.json")
 except FileNotFoundError:
     open("json_file.json", 'w')
 
 #Host a server
-host = "192.168.1.10"
-port = 12345
+host = "192.168.1.10" # use your host setting 
+port = 12345 
 buf = 1024
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(addr)
 print("Waiting to receive messages...")
+
+# creates a dict to keep your data from raspi
 newdata = {}
 newdata['sensor'] = []
 
 while True:
-    (data, addr) = UDPSock.recvfrom(buf)
+    (data, addr) = UDPSock.recvfrom(buf) # this is a function to retrieve your data from your client
+    
+    # optional codes
     print("Received message 1 : Jarak air : " + str(data.split()[-9]))
     print("Received message 2 : Tinggi air : " + str(data.split()[-5]))
     print("Received message 3 : Status gerbang : " + str(data.split()[-1]))
     data = data.decode()
-    data.split()
 
-
+    # appends your recieved data to sensor dict
     newdata['sensor'].append\
         (
             {
@@ -38,11 +41,11 @@ while True:
              'status' : data.split()[-1],
              }
         )
+    
+    # this is the function to write your data from your client as JSON file and save it to specific dir
     for data in newdata:
-        with open(r"C:\Users\ASUS ROG\PycharmProjects\little\Django\django_project\first_project\static\json_file.json", "w") as file_write:
+        with open(r"..\yourpathproject\raspi\raspiapp\static\json_file.json", "w") as file_write:
             json.dump(newdata, file_write, indent=4)
-        if data == "exit":
-            sys.exit()
 
-UDPSock.close()
-os._exit(0)
+UDPSock.close() # ignore this code
+os._exit(0) # ignore this code
